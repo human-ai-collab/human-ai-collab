bac90028610bd08ba593de97bcb25fb0d6e5111d
Daniel Dirksen <dirksend@usc.edu>
2023-03-31 18:31:52 -0700

    Added better tracing algorithm. Also, the backend can use AI to generate completions now.

--- ai.py
+++ ai.py
@@ -6,6 +6,8 @@ import torch
 import requests
 from PIL import Image
 from io import BytesIO
+import os
+import time
 
 # Might have to use this command:
 # python3 -m pip install --upgrade diffusers accelerate transformers
@@ -14,24 +16,42 @@ from diffusers import StableDiffusionImg2ImgPipeline
 # load the pipeline
 if (torch.cuda.is_available()):
   device = "cuda:0"
-  pipe = StableDiffusionImg2ImgPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16).to(
-    device
-  )
+  torch_dtype=torch.float16
+  pipe = StableDiffusionImg2ImgPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch_dtype).to(device)
 else:
   device = "cpu"
-  pipe = StableDiffusionImg2ImgPipeline.from_pretrained("runwayml/stable-diffusion-v1-5").to(
-    device
-  )
+  pipe = StableDiffusionImg2ImgPipeline.from_pretrained("runwayml/stable-diffusion-v1-5").to(device)
+pipe.safety_checker = lambda images, clip_input: (images, False)
 
-# let's download an initial image
-url = "https://raw.githubusercontent.com/CompVis/stable-diffusion/main/assets/stable-samples/img2img/sketch-mountains-input.jpg"
+def AI_complete(img):
+  """ Funtion that completes a drawing.
+  :param object img input image
+  :return completed image """
+  # url = "https://raw.githubusercontent.com/CompVis/stable-diffusion/main/assets/stable-samples/img2img/sketch-mountains-input.jpg"
+  # response = requests.get(url)
+  # img = Image.open(BytesIO(response.content)).convert("RGB")
 
-response = requests.get(url)
-init_image = Image.open(BytesIO(response.content)).convert("RGB")
-init_image.thumbnail((768, 768))
+  original_height, original_width = img.size
+  print(f"Original size: {(original_height, original_height)}")
+  downscale_size = 512
+  input_img = img.convert("RGB").resize((downscale_size, downscale_size))
+  print(f"Processing size: {input_img.size}")
+  print("Completing image...")
+  images = pipe(
+    image=input_img,
+    prompt="Intricate realistic ink drawing, trending on ArtStation, full composition, photocopy",
+    strength=0.5, # default 0.75
+    guidance_scale=7.5, # default 7.5
+    num_inference_steps=3, # default 50
+    negative_prompt="beginner, colorful", # default nothing
+    num_images_per_prompt=1 # default 1
+  ).images
 
-prompt = "A fantasy landscape, trending on artstation"
+  # if it doesn’t exist we create one
+  if not os.path.exists('output_dump'):
+    os.makedirs('output_dump')
+  images[0].save(f"output_dump/output-{int(time.time())}.png")
 
-images = pipe(prompt=prompt, image=init_image, strength=0.75, guidance_scale=7.5).images
-
-images[0].save("output/fantasy_landscape.png")
+  output = images[0].resize((original_height, original_width))
+  print(f"Output size: {output.size}")
+  return output
