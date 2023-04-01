fb1ad0e30f1bc19fcd9b3fc26c46f07560c11aca
Daniel Dirksen <dirksend@usc.edu>
2023-03-31 18:29:04 -0700

    Better Tracing algorithm, and backend can now use AI to generate completions.

--- app.py
+++ app.py
@@ -9,34 +9,53 @@ from flask import send_from_directory, request
 import base64
 from io import BytesIO
 from PIL import Image, ImageShow
+# from ai import AI_complete
 
 PORT = 8000
+USE_PLACEHOLDER = True
 
-def image_to_dataURI(img):
+# Converts PIL Image object to a dataURI PNG string.
+def PIL_to_dataURI(img):
   buffered = BytesIO()
   ext = "PNG"
   img.save(buffered, format=ext)
   base64_utf8_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
   return f"data:image/{ext};base64,{base64_utf8_str}"
 
-placeholder_img = Image.open("static/images/cat-better.png")
-placeholder_uri = image_to_dataURI(placeholder_img)
+# Converts dataURI PNG string into a PIL Image object.
+def dataURI_to_PIL(dataURI):
+  assert len(dataURI.split(",")) == 2
+  # image dataURI's follow the format data:image/png;base64,{A BUNCH OF CHARACTERS HERE}
+  # We get the data portion of this string.
+  dataPart = dataURI.split(",")[1]
+  return Image.open(BytesIO(base64.decodebytes(bytes(dataPart, "utf-8"))))
+
+placeholder_img = Image.open("static/images/cat.png")
+placeholder_uri = PIL_to_dataURI(placeholder_img)
 
 app = Flask(__name__)
 
 @app.route("/<path:path>")
 def hello_world(path):
-    dir = path or "index.html"
-    return send_from_directory("static", path)
+  dir = path or "index.html"
+  return send_from_directory("static", path)
 
 @app.route("/api/complete", methods=['POST'])
 def complete():
   body = request.json
-  input = body['image']
-  output = placeholder_uri
+  input_URI = body['image'] # dataURI for input image
+  input_PIL = dataURI_to_PIL(input_URI)
+  if (USE_PLACEHOLDER):
+    output = placeholder_uri
+  else:
+    output_PIL = AI_complete(input_PIL)
+    output_PIL.thumbnail((512, 512))
+    output_URI = PIL_to_dataURI(output_PIL)
+    output = PIL_to_dataURI(dataURI_to_PIL(output_URI))
   return {"image": output}
 
 if __name__ == "__main__":
   from waitress import serve
   print(f"serving to http://localhost:{PORT}")
+  print("READY!")
   serve(app, host="0.0.0.0", port=PORT)
